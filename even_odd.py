6b07ab1cafa01a759b83fab0ac856deed983391d
Juswaaa <72263767+Juswaaa@users.noreply.github.com>
2023-08-03 20:44:40 +0800

    Update even_odd.py

--- even_odd.py
+++ even_odd.py
@@ -1,1 +1,8 @@
+def extract_even_odd_numbers(input_file, output_even_file, output_odd_file):
+    try:
+        with open(input_file, 'r') as infile:
+            numbers = [int(num) for num in infile.read().split()]
 
+            # Filter even and odd numbers using list comprehensions
+            even_numbers = [str(num) for num in numbers if num % 2 == 0]
+            odd_numbers = [str(num) for num in numbers if num % 2 != 0]
