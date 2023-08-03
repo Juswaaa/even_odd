4bcdf6cac76c5d6ccc2012b444839b4a5f8270c3
Juswaaa <72263767+Juswaaa@users.noreply.github.com>
2023-08-03 20:45:25 +0800

    Update even_odd.py

--- even_odd.py
+++ even_odd.py
@@ -6,3 +6,14 @@ def extract_even_odd_numbers(input_file, output_even_file, output_odd_file):
             # Filter even and odd numbers using list comprehensions
             even_numbers = [str(num) for num in numbers if num % 2 == 0]
             odd_numbers = [str(num) for num in numbers if num % 2 != 0]
+            
+            # Write even numbers to the 'even.txt' file
+            with open(output_even_file, 'w') as even_file:
+                even_file.write("\n".join(even_numbers))
+
+            # Write odd numbers to the 'odd.txt' file
+            with open(output_odd_file, 'w') as odd_file:
+                odd_file.write("\n".join(odd_numbers))
+
+            print("Even numbers have been written to 'even.txt'")
+            print("Odd numbers have been written to 'odd.txt'")
